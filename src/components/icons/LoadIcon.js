e85c4901b54e02ece52f72d5028cea2fd704c1b9
Serdar Açıkyol <serdar@acikyol.net>
2021-06-06 22:59:10 +0300

    Application icons and manifest.json file generated.

--- src/components/icons/LoadIcon.js
+++ src/components/icons/LoadIcon.js
@@ -2,5 +2,14 @@ import SettingsIcon from "./SettingsIcon";
 import SearchIcon from "./SearchIcon";
 import LogoutIcon from "./LogoutIcon";
 import FlagIcon from "./FlagIcon";
+import WalletScanIcon from "./WalletScanIcon";
+import ChainIcon from "./ChainIcon";
 
-export { FlagIcon, SettingsIcon, SearchIcon, LogoutIcon };
+export {
+  WalletScanIcon,
+  FlagIcon,
+  ChainIcon,
+  SettingsIcon,
+  SearchIcon,
+  LogoutIcon,
+};
